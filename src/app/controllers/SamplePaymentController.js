27ba96b010d57b5e308c0fb2df31d96e54f1a8cb
Luis2223 <luis.santos@grupozelo.com>
2020-09-14 17:30:35 -0300

    setting .env

--- src/app/controllers/SamplePaymentController.js
+++ src/app/controllers/SamplePaymentController.js
@@ -1,11 +1,52 @@
+import Adyen from '../../config/adyen';
+
 class SamplePaymentController {
     async index(req, res) {
-        return res.status(200).json({ message: 'Hello World' });
+        try {
+            const response = await Adyen.paymentMethods({
+                channel: 'Web',
+                merchantAccount: process.env.MERCHANT_ACOUNT,
+            });
+            return res.json(response);
+        } catch (error) {
+            console.error(
+                `Error: ${error.message}, error code: ${error.errorCode}`
+            );
+            return res.status(error.statusCode).json(error.message);
+        }
     }
 
-    // async store(req, res) {
-    //     return res.status(522).send();
-    // }
+    async store(req, res) {
+        try {
+            const response = await Adyen.payments({
+                amount: {
+                    currency: 'BRL',
+                    value: 1000,
+                },
+                reference: 'Teste de token e assinatura',
+                paymentMethod: {
+                    type: 'scheme',
+                    number: '5066991111111118',
+                    expiryMonth: '03',
+                    expiryYear: '2030',
+                    cvc: '737',
+                },
+                merchantAccount: process.env.MERCHANT_ACOUNT,
+                storePaymentMethod: 'true',
+                shopperInteraction: 'Ecommerce',
+                recurringProcessingModel: 'Subscription',
+                returnUrl: 'http://localhost/',
+            });
+
+            return res.status(200).json(response);
+        } catch (error) {
+            console.error(error);
+            console.error(
+                `Error: ${error.message}, error code: ${error.errorCode}`
+            );
+            return res.status(error.statusCode).json(error.message);
+        }
+    }
 }
 
 export default new SamplePaymentController();
--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,6 @@ import SamplePaymentController from './app/controllers/SamplePaymentController';
 const routes = new Router();
 
 routes.get('/', SamplePaymentController.index);
+routes.post('/', SamplePaymentController.store);
 
 export default routes;
