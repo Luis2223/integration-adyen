8c30f46d2d117c3430d828e7c70d11d9b404b838
Luis2223 <luis.santos@grupozelo.com>
2020-09-16 16:12:18 -0300

    Create sample payment

--- src/app/controllers/SamplePaymentController.js
+++ src/app/controllers/SamplePaymentController.js
@@ -3,9 +3,16 @@ import Adyen from '../../config/adyen';
 class SamplePaymentController {
     async index(req, res) {
         try {
+            // Lista os metodos de pagamentos disponiveis
             const response = await Adyen.paymentMethods({
                 channel: 'Web',
                 merchantAccount: process.env.MERCHANT_ACOUNT,
+                countryCode: 'BR',
+                shopperLocale: 'pt-BR',
+                amount: {
+                    currency: 'BRL',
+                    value: 1000,
+                },
             });
             return res.json(response);
         } catch (error) {
@@ -18,24 +25,26 @@ class SamplePaymentController {
 
     async store(req, res) {
         try {
+            // Criando um token atraves de um pagamento
             const response = await Adyen.payments({
                 amount: {
                     currency: 'BRL',
                     value: 1000,
                 },
-                reference: 'Teste de token e assinatura',
                 paymentMethod: {
                     type: 'scheme',
-                    number: '5066991111111118',
-                    expiryMonth: '03',
-                    expiryYear: '2030',
-                    cvc: '737',
+                    encryptedSecurityCode: 'test_737',
+                    encryptedExpiryMonth: 'test_03',
+                    encryptedExpiryYear: 'test_2030',
+                    encryptedCardNumber: 'test_5066991111111118',
                 },
-                merchantAccount: process.env.MERCHANT_ACOUNT,
-                storePaymentMethod: 'true',
+                reference: '20181920',
+                shopperReference: '88032223',
+                storePaymentMethod: true,
                 shopperInteraction: 'Ecommerce',
-                recurringProcessingModel: 'Subscription',
-                returnUrl: 'http://localhost/',
+                recurringProcessingModel: 'CardOnFile',
+                returnUrl: 'https://grupozelo.com/',
+                merchantAccount: process.env.MERCHANT_ACOUNT,
             });
 
             return res.status(200).json(response);
